52e1f93b6302a9ac9a5c586785f1b22abec3e2d4
uwase-diane <diane.uwase13@gmail.com>
2021-02-18 08:18:23 -0500

    added project url

--- market/urls.py
+++ market/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^market_app/',include('market_app.urls'))
+    url(r'',include('market_app.urls')),
+    
 ]
