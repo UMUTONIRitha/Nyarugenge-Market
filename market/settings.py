91a51ebf46941c67c01c387e5dd7c535d04a7241
mutoni23 <mutoniv5@gmail.com>
2021-02-22 12:48:41 +0200

    added changes

--- market/settings.py
+++ market/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'market.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'NAME': 'market_app',
+    #     'USER': 'wecode',
+    # 'PASSWORD':'12345',
     }
 }
+    
+
 
 
 # Password validation
