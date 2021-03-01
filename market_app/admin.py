4c7726bc57e8c59f37b65baf19470398d63edcc4
UMUTONIRitha <rithamarie9@gmail.com>
2021-03-01 19:11:58 +0200

    add quantity

--- market_app/admin.py
+++ market_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from.models import Grocery,Order,OrderItem,Delivery,Category,Rate,Comment,Transaction,Profile
 # Register your models here.
-admin.site.register(Profile)
+
 admin.site.register(Grocery)
 admin.site.register(Order)
 admin.site.register(OrderItem)
